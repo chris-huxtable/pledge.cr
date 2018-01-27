7f298845de10751e24f731543863a0cff8f085b8
Chris Huxtable <chris@huxtable.ca>
2018-01-26 19:40:44 -0500

    Adds and corrects Licences

--- src/pledge.cr
+++ src/pledge.cr
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Christian Huxtable <chris@huxtable.ca>.
+# Copyright (c) 2018 Christian Huxtable <chris@huxtable.ca>.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
